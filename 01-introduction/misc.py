2a68c5acb0957e5278b71190761ab5966526ff35
ds7cw <delyan.shivachev@hotmail.com>
2025-11-26 23:54:46 +0200

    Add methods to copy elements & create models from scratch

--- 01-introduction/misc.py
+++ 01-introduction/misc.py
@@ -1,4 +1,5 @@
 import ifcopenshell
+import ifcopenshell.api.root
 import ifcopenshell.util.classification
 import ifcopenshell.util.element
 import ifcopenshell.util.placement
@@ -192,6 +193,80 @@ def print_element_distribution_system(ifc_model) -> None:
         print("This pipe is part of the system", system.Name)
 
 
+def create_copy_of_element(ifc_file, element_to_copy, mode):
+    """
+    Copy an entity instance
+    mode - 'high level'; 'shallow'; 'deepgraph'
+    """
+    if mode.lower() == 'high level':
+        return ifcopenshell.api.root.copy_class(ifc_file, product=element_to_copy)
+    elif mode.lower() == 'shallow':
+        return ifcopenshell.util.element.copy(model, element_to_copy)
+    elif mode.lower() == 'deepgraph':
+        return ifcopenshell.util.element.copy_deep(model, element_to_copy, exclude=None)
+    else:
+        print('Unknown mode attribute. Expected: "high level", "shallow" or "deepgraph"')
+        return
+
+
+def create_simple_ifc_project():
+    """Create a simple model from scratch"""
+    import ifcopenshell.api.root
+    import ifcopenshell.api.unit
+    import ifcopenshell.api.context
+    import ifcopenshell.api.project
+    import ifcopenshell.api.spatial
+    import ifcopenshell.api.geometry
+    import ifcopenshell.api.aggregate
+
+    # Create a blank model
+    model = ifcopenshell.api.project.create_file()
+
+    # All projects must have one IFC Project element
+    project = ifcopenshell.api.root.create_entity(model, ifc_class="IfcProject", name="My Project")
+
+    # Geometry is optional in IFC, but because we want to use geometry in this example, let's define units
+    # Assigning without arguments defaults to metric units
+    ifcopenshell.api.unit.assign_unit(model)
+
+    # Let's create a modeling geometry context, so we can store 3D geometry (note: IFC supports 2D too!)
+    context = ifcopenshell.api.context.add_context(model, context_type="Model")
+
+    # In particular, in this example we want to store the 3D "body" geometry of objects, i.e. the body shape
+    body = ifcopenshell.api.context.add_context(model, context_type="Model",
+        context_identifier="Body", target_view="MODEL_VIEW", parent=context)
+
+    # Create a site, building, and storey. Many hierarchies are possible.
+    site = ifcopenshell.api.root.create_entity(model, ifc_class="IfcSite", name="My Site")
+    building = ifcopenshell.api.root.create_entity(model, ifc_class="IfcBuilding", name="Building A")
+    storey = ifcopenshell.api.root.create_entity(model, ifc_class="IfcBuildingStorey", name="Ground Floor")
+
+    # Since the site is our top level location, assign it to the project
+    # Then place our building on the site, and our storey in the building
+    ifcopenshell.api.aggregate.assign_object(model, relating_object=project, products=[site])
+    ifcopenshell.api.aggregate.assign_object(model, relating_object=site, products=[building])
+    ifcopenshell.api.aggregate.assign_object(model, relating_object=building, products=[storey])
+
+    # Let's create a new wall
+    wall = ifcopenshell.api.root.create_entity(model, ifc_class="IfcWall")
+
+    # Give our wall a local origin at (0, 0, 0)
+    ifcopenshell.api.geometry.edit_object_placement(model, product=wall)
+
+    # Add a new wall-like body geometry, 5 meters long, 3 meters high, and 200mm thick
+    representation = ifcopenshell.api.geometry.add_wall_representation(model, context=body, length=5, height=3, thickness=0.2)
+    # Assign our new body geometry back to our wall
+    ifcopenshell.api.geometry.assign_representation(model, product=wall, representation=representation)
+
+    # Place our wall in the ground floor
+    ifcopenshell.api.spatial.assign_container(model, relating_structure=storey, products=[wall])
+
+    # Write out to a file
+    current_wd = os.getcwd()
+    IFC_FILE_PATH = os.path.join(current_wd, 'sample_file.ifc')
+    model.write(IFC_FILE_PATH)
+
+
 if __name__ == '__main__':
     # iterate_through_all_entities(ifc_model=model)
     # print_all_entity_types(ifc_model=model)
@@ -211,3 +286,4 @@ if __name__ == '__main__':
         ifc_model=model, instance_description=IfcElementEnum.WALL.value)
     print_element_classification(
         ifc_model=model, instance_description=IfcElementEnum.DOOR.value)
+    create_simple_ifc_project()
