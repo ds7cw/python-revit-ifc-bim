6cfb3df4710063b2157f8a6e5c8af1b00f21a2a7
ds7cw <delyan.shivachev@hotmail.com>
2025-12-07 21:28:09 +0200

    Add method to filter elements based on material

---
+++ 01-introduction/selector_syntax.py
@@ -0,0 +1,32 @@
+import ifcopenshell
+import ifcopenshell.util.selector
+import os
+
+from enum import Enum
+
+# cd into this directory before running the main.py file
+current_wd = os.getcwd()
+# Download IFC model from https://docs.ifcopenshell.org/ifcopenshell-python/hello_world.html
+IFC_FILE_NAME = 'AC20-FZK-Haus.ifc'
+IFC_FILE_PATH = os.path.join(current_wd, IFC_FILE_NAME)
+
+model = ifcopenshell.open(IFC_FILE_PATH)
+
+
+class IfcElementEnum(Enum):
+    DOOR = 'IfcDoor'
+    SLAB = 'IfcSlab'
+    WALL = 'IfcWall'
+    WINDOW = 'IfcWindow'
+
+
+def print_concrete_elements_of_categories(ifc_file, *categories, material) -> None:
+    """The material filter checks any assigned IfcMaterial with a matching name"""
+    query = ', '.join(categories) + ', material={}'.format(material)
+    asd = ifcopenshell.util.selector.filter_elements(ifc_file=ifc_file, query=query)
+    [print(el) for el in asd]
+
+
+if __name__ == '__main__':
+    print_concrete_elements_of_categories(
+        model, IfcElementEnum.DOOR.value, IfcElementEnum.WINDOW.value, material='Holz')
