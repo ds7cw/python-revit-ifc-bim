7bd427561a4ddf3a16a9b3782e3f4c9bf1383336
ds7cw <delyan.shivachev@hotmail.com>
2025-12-17 23:15:28 +0200

    Add element selection & unit conversion examples

--- 02-revit-python-shell/aa_intro.py
+++ 02-revit-python-shell/aa_intro.py
@@ -26,3 +26,37 @@ for room in col_rooms:
 # alternative 'Name' property getter: room.get_Parameter(BuiltInParameter.ROOM_NAME).AsString()
 # alternative 'Department' property getter: room.get_Parameter(BuiltInParameter.ROOM_DEPARTMENT).AsString()
 # use room.GetParameters('Name') in case there are multiple 'Name' Properties
+
+### Task Dialogs & Units Conversion
+total_area_sqf = 0
+
+for room in col_rooms:
+    total_area_sqf += room.Area
+
+print(total_area_sqf)
+
+total_area_sqm = UnitUtils.ConvertFromInternalUnits(total_area_sqf, UnitTypeId.SquareMeters)
+
+# TaskDialog Class; a dialog box that can be used to display information and receive input from the user
+message = TaskDialog
+text = "Total area of all rooms in the building:\n {} sqm\n {} sqf".format(
+    round(total_area_sqm, 1), round(total_area_sqf, 1))
+message.Show("Total Area Calculation", text)
+
+### Element Selection
+# Autodesk.Revit.UI.Selection.Selection object
+selection = uidoc.Selection # assumes the user has already selected elements in the Revit session
+elements = []
+for el_id in selection.GetElementIds():
+    elements.append(doc.GetElement(el_id))
+
+for el in elements:
+    if el.Category.Name == 'Walls':
+        print(el.IsStackedWall)
+
+active_view = uidoc.ActiveView
+col_selection = FilteredElementCollector(doc, active_view.Id). \
+    OfCategory(BuiltInCategory.OST_RoomTags).WhereElementIsNotElementType().ToElementIds()
+
+# SetElementIds selects the elements
+user_selection = uidoc.Selection.SetElementIds(col_selection) # elements in Revit will be selected
